3633c2e09610e0ad2a631e850a35fb30c9ad36d5
jeferbc <jeffe.bernal@gmail.com>
2017-10-06 17:23:48 -0500

    filtering

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import posts from './posts'
 
@@ -5,17 +6,36 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(){
+    super();
+    this.updateSearch = this.updateSearch.bind(this);
+    this.search = this.search.bind(this);
+    this.state = {
+      filterText: ""
+    }
+  }
+  updateSearch(event){
+    this.setState({
+      filterText: event.target.value
+    });
+  }
+  search(){
+    var show  = [];
+    posts.forEach((post,index) => {
+      if (post.title.toLowerCase().indexOf(this.state.filterText.toLowerCase()) === -1)
+        return;
+      show.push(<li key={index}><a href={post.url}><img src={post.image } /></a><p>{ post.title }</p></li>);
+    });
+    return show;
+    }
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.filterText} onChange={this.updateSearch} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+           {this.search()}
         </ul>
       </div>
     )
@@ -24,5 +44,3 @@ class App extends Component {
 
 
 export default App
-
-
